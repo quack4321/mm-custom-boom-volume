d0cf5b4090b895d381610106b4f22fa065e5bcbc
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-07-07 02:20:02 -0400

    Add optional dependency import and section in toml, add imports for mod folder path and mod file path

--- include/recomputils.h
+++ include/recomputils.h
@@ -22,4 +22,22 @@ RECOMP_IMPORT("*", u64 recomphook_get_return_u64());
 RECOMP_IMPORT("*", float recomphook_get_return_float());
 RECOMP_IMPORT("*", double recomphook_get_return_double());
 
+typedef enum {
+    // The dependency was found and the version requirement was met.
+    DEPENDENCY_STATUS_FOUND = 0,
+    // The ID given is not a dependency of the mod in question.
+    DEPENDENCY_STATUS_INVALID_DEPENDENCY = 1,
+    // The dependency was not found.
+    DEPENDENCY_STATUS_NOT_FOUND = 2,
+    // The dependency was found, but the version requirement was not met.
+    DEPENDENCY_STATUS_WRONG_VERSION = 3
+} DependencyStatus;
+
+// Checks the status of the provided dependency. This is only relevant for optional dependencies,
+// as the game wouldn't have started if a normal dependency wasn't met.
+// Use the return value of this to prevent calling imported functions from optional dependencies that are missing.
+// Try to avoid calling this function repeatedly, as it may incur some overhead. Instead, call it once at startup
+// and store the result into a variable for reading later on.
+RECOMP_IMPORT("*", DependencyStatus recomp_is_dependency_met(const char* dependency_id));
+
 #endif
